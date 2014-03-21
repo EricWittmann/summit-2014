f5f08eca39f851b642b267df49f485158eb2e707
Eric Wittmann <eric.wittmann@gmail.com>
2014-03-21 18:59:15 -0400

    another take on the visualization

--- summit-2014-viz/src/main/webapp/js/visualization.js
+++ summit-2014-viz/src/main/webapp/js/visualization.js
@@ -1,3 +1,29 @@
+
+SVGElement.prototype.hasClass = function (className) {
+  return new RegExp('(\\s|^)' + className + '(\\s|$)').test(this.getAttribute('class'));
+};
+
+SVGElement.prototype.addClass = function (className) {
+  if (!this.hasClass(className)) {
+    this.setAttribute('class', this.getAttribute('class') + ' ' + className);
+  }
+};
+
+SVGElement.prototype.removeClass = function (className) {
+  var removedClass = this.getAttribute('class').replace(new RegExp('(\\s|^)' + className + '(\\s|$)', 'g'), '$2');
+  if (this.hasClass(className)) {
+    this.setAttribute('class', removedClass);
+  }
+};
+
+SVGElement.prototype.toggleClass = function (className) {
+  if (this.hasClass(className)) {
+    this.removeClass(className);
+  } else {
+    this.addClass(className);
+  }
+};
+
 var w = 1024, h = 768, node, link, force, vis;
 var i = 0;
 
@@ -14,7 +40,8 @@ function update(root) {
 	});
 
 	// Enter any new links.
-	link.enter().insert("svg:line", ".node").attr("class", "link")
+	link.enter().insert("svg:line", ".node").attr("class", getLinkClass)
+	    .attr("id", getLinkId)
 		.attr("x1", function(d) { return d.source.x; })
 		.attr("y1", function(d) { return d.source.y; })
 		.attr("x2", function(d) { return d.target.x; })
@@ -26,39 +53,50 @@ function update(root) {
 	// Update the nodes...
 	node = vis.selectAll(".node").data(nodes, function(d) {
 		return d.id;
-	}).style("fill", color);
-
-//	node.transition().attr("r", function(d) {
-//		return d.children ? 25 : d.size;
-//	});
+	});
 
 	// Enter any new nodes.	
-	node.enter().append("svg:g").attr("class", "node").call(force.drag);
+	node.enter().append("svg:g").attr("class", getNodeClass).attr("id", getNodeId).call(force.drag);
 	node.html('');
 	node.append(createNodeShape)
 		.attr("cx", function(d) { return 0; })
 		.attr("cy", function(d) { return 0; })
-		.attr("r", function(d) { return nodeSize(d); })
-		.style("fill", color).on("click", click);
+		.attr("r", function(d) { return nodeSize(d); });
 
 	node.append("svg:text")
 		.attr("dx", function(d) { return nodeSize(d) + 5; })
 		.attr("dy", ".35em")
 		.attr("class", "node-label")
-		.text(function(d) { return d.name == 'root' ? '' : d.name; });
+		.text(function(d) { return d.type == 'profile' ? d.name : ''; });
 
 	// Remove any old nodes.
 	node.exit().remove();
+}
+
+function getLinkId(d) {
+	return 'link-' + d.target.id;
+}
+
+function getNodeId(d) {
+	return 'node-' + d.id;
+}
+
+function getNodeClass(d) {
+	return 'node node-' + d.type + (d.active ? ' node-active' : '');
+}
+
+function getLinkClass(d) {
+	return 'link link-to-' + d.target.type + (d.target.active ? ' link-active' : '');
 }
 
 function createNodeShape(d) {
 	var shape;
 	if (d.type == 'root') {
 		shape = this.ownerDocument.createElementNS("http://www.w3.org/2000/svg", "rect");
-		shape.setAttribute('x', 0);
-		shape.setAttribute('y', 0);
-		shape.setAttribute('width', 0);
-		shape.setAttribute('height', 0);
+		shape.setAttribute('x', -20);
+		shape.setAttribute('y', -20);
+		shape.setAttribute('width', 40);
+		shape.setAttribute('height', 40);
 	} else {
 		shape = this.ownerDocument.createElementNS("http://www.w3.org/2000/svg", "circle");
 		shape.setAttribute('cx', 0);
@@ -73,11 +111,7 @@ function nodeSize(d) {
 		return 30;
 	}
 	if (d.type == 'profile') {
-		if (isCollapsed(d)) {
-			return 15;
-		} else {
-			return 25;
-		}
+		return 25;
 	}
 	return d.size;
 }
@@ -98,19 +132,7 @@ function tick() {
 
 // Color leaf nodes orange, and packages white or blue.
 function color(d) {
-	return isCollapsed(d) ? "#f1ac6f" : d.children ? "#c6dbef" : "#429cd5";
-}
-
-// Toggle children on click.
-function click(d) {
-	if (d.children) {
-		d._children = d.children;
-		d.children = null;
-	} else {
-		d.children = d._children;
-		d._children = null;
-	}
-	update();
+	return d.children ? "#c6dbef" : "#429cd5";
 }
 
 // Returns a list of all nodes under the root.
@@ -132,10 +154,41 @@ function flatten(root) {
 	return nodes;
 }
 
-function isCollapsed(d) {
-	return d._children;
+
+function activateGear(whichGear) {
+    var gearId = 'node-' + whichGear;
+    var linkId = 'link-' + whichGear;
+    var gear = document.getElementById(gearId);
+    var link = document.getElementById(linkId);
+    if (gear && link) {
+	    gear.addClass('node-active');
+	    gear.addClass('pulse');
+	    link.addClass('link-active');
+	    link.addClass('pulse');
+	    setTimeout(function () {
+	        gear.removeClass('pulse');
+	        link.removeClass('pulse');
+	    }, 5600);
+    }
 }
 
+
+function deactivateGear(whichGear) {
+    var gearId = 'node-' + whichGear;
+    var linkId = 'link-' + whichGear;
+    var gear = document.getElementById(gearId);
+    var link = document.getElementById(linkId);
+    if (gear && link) {
+	    gear.removeClass('node-active');
+	    gear.addClass('pulse');
+	    link.removeClass('link-active');
+	    link.addClass('pulse');
+	    setTimeout(function () {
+	        gear.removeClass('pulse');
+	        link.removeClass('pulse');
+	    }, 5600);
+    }
+}
 
 $(document).ready(
 		function() {
